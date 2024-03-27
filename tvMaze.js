7915929d574d8af81e00a4e0d360c738266a9b23
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-03-27 13:49:06 +0330

    feat : add-10-cards ; adding 10 cards in main

--- tvMaze.js
+++ tvMaze.js
@@ -1,9 +1,12 @@
 const valueOfInput = document.querySelector(".searchBox");
 const searchIcon = document.querySelector(".searchIcon");
+const imgCArd = document.querySelectorAll(".img-cards");
+const cardTitle = document.querySelectorAll(".card-title");
+const genre = document.querySelectorAll(".genre");
+const imdb = document.querySelectorAll(".imdb");
 
 valueOfInput.addEventListener("keyup", (event) => {
   event.preventDefault();
-  console.log(valueOfInput.value);
   const query = valueOfInput.value;
 
   if (!event.value) {
@@ -13,16 +16,32 @@ valueOfInput.addEventListener("keyup", (event) => {
   const getData = async () => {
     const res = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
     const data = await res.json();
-    // return data;
+    return data;
   };
 
   getData()
-    .then((res) => res)
-    .then((data) => {
-      data.forEach((el) => {
-        console.log(el);
-        // document.createElement("")
-      });
-    })
+    .then((data) => info(data))
     .catch((er) => console.log(er));
 });
+
+const info = (data) => {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < imgCArd.length; j++) {
+
+      const shows = data[j].show;
+      if (data[j].show.image !== null) {
+        imgCArd[j].removeAttribute("src");
+        imgCArd[j].src = shows.image.medium;
+        cardTitle[j].innerHTML = shows.name;
+        genre[j].innerHTML = shows.genres.join().replaceAll(",", " | ");
+      }
+
+      if (shows.rating.average !== null) {
+        imdb[j].innerHTML = shows.rating.average;
+      } else {
+        imdb[j].innerHTML = "Movie information not found";
+      }
+
+    }
+  }
+};
