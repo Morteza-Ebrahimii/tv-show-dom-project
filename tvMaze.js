43d45b63cba226b5bad5dce975d8b5981254f3ad
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-04-01 14:03:10 +0330

    finish-basic-card ; finish the basic cards in index

--- tvMaze.js
+++ tvMaze.js
@@ -5,22 +5,43 @@ const cardTitle = document.querySelectorAll(".card-title");
 const genre = document.querySelectorAll(".genre");
 const imdb = document.querySelectorAll(".imdb");
 
+
+// -------------- basic card in the page ----------------
 const getDataShows = async () => {
   const res = await fetch("https://api.tvmaze.com/shows");
   const data = await res.json();
   return data;
 };
 
-const getRandom = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-Array.from({ length: 10 }, () => console.log(getRandom(0, 240)));
-
 getDataShows()
   .then((res) => res)
-  .then((data) => console.log(data))
+  .then((data) => showsData(data))
   .catch((er) => console.log(er));
 
+let counter = true;
+
+const showsData = (data) => {
+  if (counter) {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < imgCArd.length; j++) {
+        imgCArd[j].src = data[j].image.original;
+        cardTitle[j].innerHTML = data[j].name;
+        genre[j].innerHTML = data[j].genres.join().replaceAll(",", " | ");
+
+        if (data[j].rating.average !== null) {
+          imdb[j].innerHTML = data[j].rating.average;
+        } else {
+          imdb[j].innerHTML = "Movie information not found";
+        }
+      }
+    }
+  }
+};
+
+
+// -------------- with search card in the page ----------------
 valueOfInput.addEventListener("keyup", (event) => {
+  counter = false;
   event.preventDefault();
   const query = valueOfInput.value;
 
@@ -42,13 +63,14 @@ valueOfInput.addEventListener("keyup", (event) => {
 const infoOfQuery = (data) => {
   for (let i = 0; i < data.length; i++) {
     for (let j = 0; j < imgCArd.length; j++) {
+      // imgCArd[j] = ""
       const shows = data[j].show;
       if (data[j].show.image !== null) {
         imgCArd[j].removeAttribute("src");
         imgCArd[j].src = shows.image.medium;
         cardTitle[j].innerHTML = shows.name;
         genre[j].innerHTML = shows.genres.join().replaceAll(",", " | ");
-        console.log(data[j]);
+        console.log(shows);
       }
 
       if (shows.rating.average !== null) {
