0aec6a6f72367dba78d3ad11ff67126bbbc65754
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-05-08 15:03:09 +0330

    delete: delete js file: change strategy in js file

--- tvMaze.js
+++ tvMaze.js
@@ -15,79 +15,11 @@ const getDataShows = async () => {
 
 getDataShows()
   .then((res) => res)
-  .then((data) => showsData(data))
+  .then((data) => getData(data))
   .catch((er) => console.log(er));
 
-let counter = true;
-
-const showsData = (data) => {
-  if (counter) {
-    for (let i = 0; i < 10; i++) {
-      // for show details of each movies for basic movies in the page
-      cards[i].addEventListener("click", (e) => {
-        const url = data[i].url;
-        window.open(url, "_blank");
-      });
-
-      for (let j = 0; j < imgCArd.length; j++) {
-        imgCArd[j].src = data[j].image.original;
-        cardTitle[j].innerHTML = data[j].name;
-        genre[j].innerHTML = data[j].genres.join().replaceAll(",", " | ");
-
-        if (data[j].rating.average !== null) {
-          imdb[j].innerHTML = data[j].rating.average;
-        } else {
-          imdb[j].innerHTML = "Movie information not found";
-        }
-      }
-    }
-  }
-};
-
-// -------------- with search card in the page ----------------
-valueOfInput.addEventListener("keyup", (event) => {
-  counter = false;
-  event.preventDefault();
-  const query = valueOfInput.value;
-
-  if (!event.value) {
-    searchIcon.style.visibility = "hidden";
-  }
-
-  const getDataQuery = async () => {
-    const res = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
-    const data = await res.json();
-    return data;
-  };
-
-  getDataQuery()
-    .then((data) => infoOfQuery(data))
-    .catch((er) => console.log(er));
-});
-
-const infoOfQuery = (data) => {
-  for (let i = 0; i < data.length; i++) {
-    
-    // for show details of each movies when use search box
-    cards[i].addEventListener("click", (e) => {
-      const url = data[i].show.url;
-      window.open(url, "_blank");
-    });
-
-    for (let j = 0; j < imgCArd.length; j++) {
-      const shows = data[j].show;
-      if (data[j].show.image !== null) {
-        imgCArd[j].removeAttribute("src");
-        imgCArd[j].src = shows.image.medium;
-        cardTitle[j].innerHTML = shows.name;
-        genre[j].innerHTML = shows.genres.join().replaceAll(",", " | ");
-      }
-
-      if (shows.rating.average !== null) {
-        imdb[j].innerHTML = shows.rating.average;
-      } else {
-        imdb[j].innerHTML = "Movie information not found";
-      }
-    }
-  }
+const getData = (data) => {
+  data.forEach((el) => {
+    console.log(el);
+  });
 };
